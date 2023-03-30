e574a065eccc48261b1ea113c8c269b274a4671a
MarcP70 <marcpourias@yahoo.fr>
2023-03-30 07:59:16 -0700

    Modified the emplacement of the include <stdarg.h>

--- main.h
+++ main.h
@@ -7,6 +7,8 @@
  * Desc: Header file containing prototypes for all functions
  */
 
+#include <stdarg.h>
+
 /* Functions prototype */
 int _putchar(char c);
 int _print_char(va_list arg);
@@ -14,6 +16,7 @@ int _print_string(va_list arg);
 int _print_int(va_list arg);
 
 /* Structures */
+
 /**
  * <stdarg> is a arguments library
  * struct printer - A new struct type defining assiotiation forma specifer.
@@ -21,8 +24,6 @@ int _print_int(va_list arg);
  * @fonction: the function associated
  * @printer_t: alias of the structure
  */
-
-#include <stdarg.h>
 
 typedef struct printer
 {
