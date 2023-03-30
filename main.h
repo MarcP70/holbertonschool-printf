e7d76d1bc4a0cf11f383c5c8806c37564023ccc7
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-03-30 11:22:55 +0200

    create and arengement files main.h

--- main.h
+++ main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
 int _putchar(char c);
+int _print_char(va_list arg);
+int _print_string(va_list arg);
+int _print_int(va_list arg);
 
 typedef struct printer
 {
