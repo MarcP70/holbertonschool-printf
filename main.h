b190eaf3b94bdf7d916e9146316be80eb8d5da06
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-03-30 15:57:45 +0200

    added commente on the head

--- main.h
+++ main.h
@@ -1,13 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
+/*
+ * File: main.h
+ * Auth:  Gaetan-Lecuyer
+ * Desc: Header file containing prototypes for all functions
+ */
 
-int _printf(const char *format, ...);
 int _putchar(char c);
 int _print_char(va_list arg);
 int _print_string(va_list arg);
 int _print_int(va_list arg);
+
+/**
+ * <stdarg> is a arguments library
+ * struct printer - A new struct type defining assiotiation forma specifer.
+ * @symbol: A symbol representing a data type.
+ * @fonction: the associated 
+ * @printer_t: alias of the structure 
+ */
+
+#include <stdarg.h>
 
 typedef struct printer
 {
