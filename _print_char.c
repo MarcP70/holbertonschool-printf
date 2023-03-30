c3e4550d6bb3072b8df8099c24df0e074c49dd58
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-03-30 16:14:19 +0200

    added commente file _print_char.c

--- _print_char.c
+++ _print_char.c
@@ -1,6 +1,15 @@
+/*
+ * File: _print_char.c
+ * Auth: Lecueyer Gaetan
+ */
 #include <stdarg.h>
 #include "main.h"
 
+/**
+ * print_char - Prints a char.
+ * @char: is the variable pointed to by the string
+ * @arg: letter = arguments of int
+ */
 int _print_char(va_list arg)
 {
 	char letter;
