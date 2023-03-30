8601c0fb18b7138ceb02cee8518855042931e061
MarcP70 <marcpourias@yahoo.fr>
2023-03-30 07:39:12 -0700

    Fixed indent on line 5

--- _print_string.c
+++ _print_string.c
@@ -1,16 +1,21 @@
 /*
  * File: _print_string.c
  * Auth: Lecueyer Gaetan
+ *       Marc Pourias
+ *       Chaherdine Abdou
  */
+
 #include <stdarg.h>
 #include <stddef.h>
 #include "main.h"
 
 /**
- * print_string - Prints a string.  
+ * _print_string - Prints a string.
  * @arg: A list of arguments pointing to
  *       the string to be printed.
+ * Return: the lenght of the string
  */
+
 int _print_string(va_list arg)
 {
 	char *str;
