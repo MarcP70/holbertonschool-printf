f41346faa38ca3319af5880bdb5ca9d10625cdd8
MarcP70 <marcpourias@yahoo.fr>
2023-03-30 07:35:16 -0700

    Fixed missing blanc line after declaration

--- _print_char.c
+++ _print_char.c
@@ -1,18 +1,22 @@
-
 /*
  * File: _print_char.c
  * Auth: Lecueyer Gaetan
+ *       Marc Pourias
+ *       Chaherdine Abdou
  */
+
 #include <stdarg.h>
 #include "main.h"
 
 /**
- * print_char - Prints a char.
+ * _print_char - Prints a char.
  * @arg: letter to print
+ * Return: 1 - lenght of one caracter
  */
 int _print_char(va_list arg)
 {
 	char letter;
+
 	letter = va_arg(arg, int);
 	_putchar(letter);
 	return (1);
