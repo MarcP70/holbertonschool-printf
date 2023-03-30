cd8450a6ea814679bdebcee4e431c43fe6392c1c
chaher13 <chahercrbs@gmail.com>
2023-03-30 07:05:11 -0700

    Fixed trailing whitespaces

--- main.h
+++ main.h
@@ -3,21 +3,23 @@
 
 /*
  * File: main.h
- * Auth:  Gaetan-Lecuyer
+ * Auth:  Gaetan Lecuyer/Marc Pourias/Chaherdine Abdou
  * Desc: Header file containing prototypes for all functions
  */
 
+/* Functions prototype */
 int _putchar(char c);
 int _print_char(va_list arg);
 int _print_string(va_list arg);
 int _print_int(va_list arg);
 
+/* Structures */
 /**
  * <stdarg> is a arguments library
  * struct printer - A new struct type defining assiotiation forma specifer.
- * @symbol: A symbol representing a data type.
- * @fonction: the associated 
- * @printer_t: alias of the structure 
+ * @symbol: the symbol representing a data type.
+ * @fonction: the function associated
+ * @printer_t: alias of the structure
  */
 
 #include <stdarg.h>
